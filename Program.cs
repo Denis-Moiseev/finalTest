ac1759344c37a89356992dd724375352c6e32a76
Denis-Moiseev <mrtesla666@gmail.com>
2023-11-30 20:06:26 +0800

    addCode

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = { "Hello", "2", "world", ":-)" };
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] newArray = new string[count];
+int index = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[index] = array[i];
+        index++;
+    }
+}
+
+Console.Write("[");
+for (int i = 0; i < newArray.Length; i++)
+{
+    Console.Write("\"" + newArray[i] + "\"");
+    if (i < newArray.Length - 1)
+    {
+        Console.Write(", ");
+    }
+}
+Console.WriteLine("]");
